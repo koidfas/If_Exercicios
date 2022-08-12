57ec8fac1d6fed40b245c268cc4bf6166950e2c8
Leonardo <leonardo@leonardo-ideapad.localdomain>
2022-08-12 15:28:17 -0300

    Faltou um KKKKKKKKKKKKKK

--- Exercicios_1/Exercicios-13.c
+++ Exercicios_1/Exercicios-13.c
@@ -10,8 +10,8 @@ int main(){
     printf("Digite o numero de votos válidos:");
     scanf("%d", &votos_válidos);
 
-    printf("De %d votos %d%% são de votos brancos!",  );
-    printf("De %d votos %d%% são de votos válidos!", );
+    printf("De %d votos %d%% são de votos brancos!\n", votos_brancos ,(votos_brancos * 100) / 100 );
+    printf("De %d votos %d%% são de votos válidos!\n", votos_válidos ,(votos_válidos * 100) / 100 );
 
     return 0;
 }
